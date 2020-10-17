004d045c5dffc31e7ff837ac65436bc5d27e88ab
rachana15 <rachana.bhaskar@gmail.com>
2020-10-17 13:32:13 -0500

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbozLE_zrtwpTpT8yMOsawGTlyXTOSC0w",
-  authDomain: "chat-app-7e607.firebaseapp.com",
-  databaseURL: "https://chat-app-7e607.firebaseio.com",
-  projectId: "chat-app-7e607",
-  storageBucket: "chat-app-7e607.appspot.com",
-  messagingSenderId: "334552605849",
-  appId: "1:334552605849:web:2e2dc6f78319969b818408"
+  //
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
